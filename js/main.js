a6817eaca772fa8b450467568a216b47f2f5cbf4
Lennox Mwabonje <lenblazy@gmail.com>
2025-01-08 19:57:03 +0300

    Access an element innehtlm

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var btn = document.getElementById("go-button");
 function buttonClick() {
   alert("You clicked the button");
   btn.removeEventListener("click", buttonClick);
+  document.getElementById("text").innerHTML = "Don't do it";
 }
 
 
