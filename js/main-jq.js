85458469f6546372369eb625618c4acb41f5e918
Lennox Mwabonje <lenblazy@gmail.com>
2025-01-10 12:46:21 +0300

    target elements

--- js/main-jq.js
+++ js/main-jq.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
-  alert('jQuery is working!');
+  // vanilla JS
+  // document.getElementById('text').innerHTML = 'This is now my text';
+  $('#text').html('This is now my text'); //jquery
+
+  // vanilla JS
+  // document.getElementsByClassName('my-input')[0].value = 'New Input Value';
+  $('.my-input').val('New Input Value'); //jquery
+
 
  });
