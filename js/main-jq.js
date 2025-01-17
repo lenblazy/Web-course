e2f442724658cccbe0dd05152d53f6a4c6c523b1
Lennox Mwabonje <lenblazy@gmail.com>
2025-01-17 12:47:44 +0300

    Use css in jquery

--- js/main-jq.js
+++ js/main-jq.js
@@ -1,146 +1,150 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  gQuery('my-selector').addClass('new-class');
-
-  console.log($('.submenu a').first().text());
-  console.log($('.submenu a').last().text());
-
-  $(document).on('contextmenu', function(event) {
-    return false;
-  });
-
- 
-
-  $(document).on('mousedown', function(event) {
-    event.stopPropagation();
-
-    if (event.which === 2) {
-
-      $('.hidden').removeClass('shown');
-
-      if($(event.target).is('img')) {
-        $('.saveimg, .newtab').addClass('shown');
-      } else if($(event.target).is('a')) {
-        $('.newtab').addClass('shown');
-      }
-
-
-      $('#context').css({
-        top: event.pageY,
-        left: event.pageX
-      });
-
-      $('#context').fadeIn();
-      return false;
-
-     
+    function printThis(el) {
+        console.log(' hahahaha');
+        console.log($(el).text());
     }
 
-    $('#context').fadeOut();
+    gQuery('my-selector').addClass('new-class');
 
-    // switch (event.which) {
-    //   case 1:
-    //     console.log('Left mouse button pressed');
-    //     break;
-    //   case 2:
-    //     console.log('Middle mouse button pressed');
-    //     break;
-    //   case 3:
-    //     console.log('Right mouse button pressed');
-    //     break;
-    //   default:
-    //     console.log('Unknown mouse button pressed');
-    // }
-  });
+    console.log($('.submenu a').first().text());
+    console.log($('.submenu a').last().text());
 
-  $('[href="https://www.google.com"]').on('click', function(event) {
-    // event.preventDefault();
-    return false;
-    console.log('Google link clicked');
-  });
-
-  // // vanilla JS
-  // // document.getElementById('text').innerHTML = 'This is now my text';
-  // $('#text').html('This is now my text'); //jquery
-
-  // // vanilla JS
-  // // document.getElementsByClassName('my-input')[0].value = 'New Input Value';
-  // $('.my-input').val('New Input Value'); //jquery
-
-  // var el = document.getElementById('text');
-
-  // console.log("EL in Vanilla JS:", el);
-  // console.log("EL in jQuery:", $(el));
-
-  // $('#go-button').on('click', function() {
-  //   console.log('Button clicked');
-  // });
-
-  $('[data-trigger="dropdown"]').on('mouseenter', function() {
-    var submenu = $(this).parent().find('.submenu');
-    submenu.fadeIn(300);
-
-    $('.profile-menu').on('mouseleave', function() {
-      submenu.fadeOut(300);
+    $(document).on('contextmenu', function (event) {
+        return false;
     });
-  });
 
-  $('#prepend, #append, #replace').on('click', function(e) {
-    var el = $(e.currentTarget);
-    var action = el.attr('id');
 
-    var content = $('.text').val();
+    $(document).on('mousedown', function (event) {
+        event.stopPropagation();
 
-    if (action === 'prepend') {
-      console.log('Prepend');
-      $('#main').prepend(content);
-    } else if (action === 'append') {
-      console.log('Append');
-      $('#main').append(content);
-    } else if (action === 'replace') {
-      console.log('Replace');
-      $('#main').html(content);
+        if (event.which === 2) {
+
+            $('.hidden').removeClass('shown');
+
+            if ($(event.target).is('img')) {
+                $('.saveimg, .newtab').addClass('shown');
+            } else if ($(event.target).is('a')) {
+                $('.newtab').addClass('shown');
+            }
+
+
+            $('#context').css({
+                top: event.pageY,
+                left: event.pageX
+            });
+
+            $('#context').fadeIn();
+            return false;
+
+
+        }
+
+        $('#context').fadeOut();
+
+        // switch (event.which) {
+        //   case 1:
+        //     console.log('Left mouse button pressed');
+        //     break;
+        //   case 2:
+        //     console.log('Middle mouse button pressed');
+        //     break;
+        //   case 3:
+        //     console.log('Right mouse button pressed');
+        //     break;
+        //   default:
+        //     console.log('Unknown mouse button pressed');
+        // }
+    });
+
+    $('[href="https://www.google.com"]').on('click', function (event) {
+        // event.preventDefault();
+        return false;
+        console.log('Google link clicked');
+    });
+
+    // // vanilla JS
+    // // document.getElementById('text').innerHTML = 'This is now my text';
+    // $('#text').html('This is now my text'); //jquery
+
+    // // vanilla JS
+    // // document.getElementsByClassName('my-input')[0].value = 'New Input Value';
+    // $('.my-input').val('New Input Value'); //jquery
+
+    // var el = document.getElementById('text');
+
+    // console.log("EL in Vanilla JS:", el);
+    // console.log("EL in jQuery:", $(el));
+
+    // $('#go-button').on('click', function() {
+    //   console.log('Button clicked');
+    // });
+
+    $('[data-trigger="dropdown"]').on('mouseenter', function () {
+        var submenu = $(this).parent().find('.submenu');
+        submenu.fadeIn(300);
+
+        $('.profile-menu').on('mouseleave', function () {
+            submenu.fadeOut(300);
+        });
+    });
+
+    $('#prepend, #append, #replace').on('click', function (e) {
+        var el = $(e.currentTarget);
+        var action = el.attr('id');
+
+        var content = $('.text').val();
+
+        if (action === 'prepend') {
+            console.log('Prepend');
+            $('#main').prepend(content);
+        } else if (action === 'append') {
+            console.log('Append');
+            $('#main').append(content);
+        } else if (action === 'replace') {
+            console.log('Replace');
+            $('#main').html(content);
+        }
+
+        $('.text').val('');
+    });
+
+    $('p:contains("Lorem")').html("This has Lorem in it. Shocker");
+
+    if ($(':contains("Lorem")').is("div")) {
+        console.log("Lorem is inside a paragraph");
     }
 
-    $('.text').val('');
-  });
-
-  $('p:contains("Lorem")').html("This has Lorem in it. Shocker");
-
-  if($(':contains("Lorem")').is("div")) {
-    console.log("Lorem is inside a paragraph");
-  }
-
-  if($(':contains("Lorem")').hasClass("div")) {
-    console.log("Lorem is inside a paragraph");
-  }
-
-  $('p').each(function () {
-    console.log($(this).text());
-  });
-
-  console.
-
-  $('textarea').focusin(function() {
-    console.log('focus in on text area');
-  });
-
-  $('textarea').focusout(function() {
-    console.log('focus out of text area');
-  });
-
-  $('input').focusout(function() {
-
-    if($(this).val().indexOf('@') > -1 && $(this).val().indexOf('.') > -1 ) {
-      $('.validation').html("Valid Email");
-      console.log('Valid Email');
-    } else {
-      $('.validation').html("Your Email is Invalid");
-      console.log('Your Email is Invalid');
+    if ($(':contains("Lorem")').hasClass("div")) {
+        console.log("Lorem is inside a paragraph");
     }
-  });
 
+    $('p').each(printThis);
 
+    $('textarea').focusin(function () {
+        console.log('focus in on text area')
+    });
+
+    $('textarea').focusout(function () {
+        console.log('focus out of text area');
+    });
+
+    $('input').focusout(function () {
+
+        if ($(this).val().indexOf('@') > -1 && $(this).val().indexOf('.') > -1) {
+            $('.validation').html("Valid Email");
+            console.log('Valid Email');
+        } else {
+            $('.validation').html("Your Email is Invalid");
+            console.log('Your Email is Invalid');
+        }
+    });
+
+    $('input').css({
+        background: '#ff0',
+        padding: '10px',
+        borderColor: '#000',
+    });
 
 });
 
